5ab181d88579a701c217e74e6669734a67827cd5
Simon Maurer <moirelein@gmail.com>
2013-01-29 22:50:48 +0100

    ex 2 of assignment 1

---
+++ ex/ex_1_2.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+Entity Sum_of_Product is
+    Port (
+        w0, w1, s: IN std_logic;
+        f: OUT std_logic
+    );
+End Sum_of_Product;
+
+Architecure dataflow of Sum_of_Product:
+Signal S1, S2: std_logic;
+Begin
+    S1 <= w0 AND NOT s;
+    S2 <= w1 AND s;
+    f <= S1 OR S2;
+End dataflow;
