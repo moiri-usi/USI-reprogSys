17b6e5157b10b9f5352a7a07083188fbc076765b
Simon Maurer <moirelein@gmail.com>
2013-01-30 18:55:36 +0100

    use internal signals to generate pulses

--- project/vhdl/ctrl_unit.vhd
+++ project/vhdl/ctrl_unit.vhd
@@ -20,15 +20,22 @@ end ctrl_unit;
 architecture sm of ctrl_unit is
     type state is (init, read, mult, extract, assemble, display);
     signal current_state, next_state : state;
+    signal enable_int, enable_add_int, flush_int, load_mult_int, load_mant_int, ready_int : std_logic;
 begin
     process (current_state, reset, start, clk, ready_mult, ready_mant)
     begin
         load_mult <= '0';
+        load_mult_int <= '0';
         load_mant <= '0';
+        load_mant_int <= '0';
         enable <= '0';
+        enable_int <= '0';
         enable_add <= '0';
+        enable_add_int <= '0';
         ready <= '0';
+        ready_int <= '0';
         flush <= '0';
+        flush_int <= '0';
         next_state <= init;
         case current_state is
             when init =>
@@ -39,18 +46,33 @@ begin
                 end if;
             when read =>
                 next_state <= mult;
-                enable <= '1';
-                flush <= '1';
+                if enable_int='0' then
+                    enable_int <= '1';
+                    enable <= '1';
+                end if;
+                if flush_int='0' then
+                    flush_int <= '1';
+                    flush <= '1';
+                end if;
             when mult =>
-                load_mult <= '1';
+                if load_mult_int='0' then
+                    load_mult_int <= '1';
+                    load_mult <= '1';
+                end if;
                 if ready_mult='1' then
                     next_state <= extract;
                 else
                     next_state <= mult;
                 end if;
             when extract =>
-                enable_add <= '1';
-                load_mant <= '1';
+                if enable_add_int='0' then
+                    enable_add_int <= '1';
+                    enable_add <= '1';
+                end if;
+                if load_mant_int='0' then
+                    load_mant_int <= '1';
+                    load_mant <= '1';
+                end if;
                 if ready_mant='1' then
                     next_state <= assemble;
                 else
@@ -61,7 +83,10 @@ begin
                 -- wait one clock cycle
                 next_state <= display;
             when display =>
-                ready <= '1';
+                if ready_int='0' then
+                    ready_int <= '1';
+                    ready <= '1';
+                end if;
                 if rising_edge(start) then
                     next_state <= read;
                 else
