f10b0bb071570093d0d264be9f704429872e4ab0
Simon Maurer <moirelein@gmail.com>
2013-01-30 19:28:29 +0100

    use dummy states to generate pulses (remove internal signals again)

--- project/vhdl/ctrl_unit.vhd
+++ project/vhdl/ctrl_unit.vhd
@@ -6,9 +6,9 @@ entity ctrl_unit is
         clk         : in std_logic;
         reset       : in std_logic;
         start       : in std_logic;
-        ready_mult  : in std_logic;
+        ready_multi  : in std_logic;
         ready_mant  : in std_logic;
-        load_mult   : out std_logic;
+        load_multi   : out std_logic;
         load_mant   : out std_logic;
         enable      : out std_logic;
         enable_add  : out std_logic;
@@ -18,24 +18,17 @@ entity ctrl_unit is
 end ctrl_unit;
 
 architecture sm of ctrl_unit is
-    type state is (init, read, mult, extract, assemble, display);
+    type state is (init, read, mult, mult_dmy, extract, extract_dmy, assemble, display, display_dmy);
     signal current_state, next_state : state;
-    signal enable_int, enable_add_int, flush_int, load_mult_int, load_mant_int, ready_int : std_logic;
 begin
-    process (current_state, reset, start, clk, ready_mult, ready_mant)
+    process (current_state, reset, start, clk, ready_multi, ready_mant)
     begin
-        load_mult <= '0';
-        load_mult_int <= '0';
+        load_multi <= '0';
         load_mant <= '0';
-        load_mant_int <= '0';
         enable <= '0';
-        enable_int <= '0';
         enable_add <= '0';
-        enable_add_int <= '0';
         ready <= '0';
-        ready_int <= '0';
         flush <= '0';
-        flush_int <= '0';
         next_state <= init;
         case current_state is
             when init =>
@@ -46,51 +39,45 @@ begin
                 end if;
             when read =>
                 next_state <= mult;
-                if enable_int='0' then
-                    enable_int <= '1';
-                    enable <= '1';
-                end if;
-                if flush_int='0' then
-                    flush_int <= '1';
-                    flush <= '1';
-                end if;
+                enable <= '1';
+                flush <= '1';
             when mult =>
-                if load_mult_int='0' then
-                    load_mult_int <= '1';
-                    load_mult <= '1';
-                end if;
-                if ready_mult='1' then
+                enable <= '0';
+                flush <= '0';
+                load_multi <= '1';
+                next_state <= mult_dmy;
+            when mult_dmy =>
+                load_multi <= '0';
+                if ready_multi='1' then
                     next_state <= extract;
                 else
-                    next_state <= mult;
+                    next_state <= mult_dmy;
                 end if;
             when extract =>
-                if enable_add_int='0' then
-                    enable_add_int <= '1';
-                    enable_add <= '1';
-                end if;
-                if load_mant_int='0' then
-                    load_mant_int <= '1';
-                    load_mant <= '1';
-                end if;
+                enable_add <= '1';
+                load_mant <= '1';
+                next_state <= extract_dmy;
+            when extract_dmy =>
+                enable_add <= '0';
+                load_mant <= '0';
                 if ready_mant='1' then
                     next_state <= assemble;
                 else
-                    next_state <= extract;
+                    next_state <= extract_dmy;
                 end if;
             when assemble =>
                 -- TODO: is this state needed?
                 -- wait one clock cycle
                 next_state <= display;
             when display =>
-                if ready_int='0' then
-                    ready_int <= '1';
-                    ready <= '1';
-                end if;
+                ready <= '1';
+                next_state <= display_dmy;
+            when display_dmy =>
+                ready <= '0';
                 if rising_edge(start) then
                     next_state <= read;
                 else
-                    next_state <= display;
+                    next_state <= display_dmy;
                 end if;
         end case;
     end process;
